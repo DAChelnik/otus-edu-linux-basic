df4d796f9f8cb07b5e4c67a888334d72e21e08d2
Denis A Chelnik <denis@chelnik.ru>
2023-07-09 22:49:18 +0300

    First commit

---
+++ nodes/provision/virtualhost-create-virtualmin.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+
+# Примечания:
+#   В конце каждой команды вы можете увидеть > / dev / null . Это просто подавляет вывод из процессов установки.
+#   Если вы хотите увидеть результат при подготовке, просто удалите его.
+#   Когда вы пытаетесь установить пакет с помощью команды apt-get install , он всегда запрашивает подтверждение, 
+#   флаг -y указывает «да», поэтому он не будет запрашивать подтверждение каждой установки.
+
+sudo virtualmin create-domain --domain $ServerName --pass $VirtualminAdminPassword --desc $VirtualminServerDesc  --user $VirtualminAdmin --unix --dir --webmin --web
+# sudo virtualmin enable-domain --domain $ServerName
